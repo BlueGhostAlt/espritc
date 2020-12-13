5eb62e76e975784e8f40516a44b0a6d7e232f95b
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-12-13 12:17:59 +0200

    chore: renamed language to esprit

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import { add } from "./add"
 
 const one = Promise.resolve(1)
 
-console.log(`Hello from lily! PI = ${add(await one, 2.14)}`)
+console.log(
+    `Hello from esprit! PI = ${add(await one, 2.14)}`
+)
